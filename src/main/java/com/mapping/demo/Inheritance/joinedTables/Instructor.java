2f52d7ea99fe4e47fce8931f12eb8d5fec0a7f43
muralivsbv <muralivsbv@gmail.com>
2025-03-11 16:34:38 +0530

    small comment added

--- src/main/java/com/mapping/demo/Inheritance/joinedTables/Instructor.java
+++ src/main/java/com/mapping/demo/Inheritance/joinedTables/Instructor.java
@@ -3,7 +3,7 @@ package com.mapping.demo.Inheritance.joinedTables;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 
-
+//newly added branch we are pushing now- main branch
 @PrimaryKeyJoinColumn(name="user_id")
 @Entity(name="jt_Instructor")
 public class Instructor extends User {
